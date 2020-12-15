7a7210084c9c75c68659980ccfb44f47b8a552b5
AbdelhakRazi <ja_razi@esi.dz>
2020-12-15 11:28:58 +0100

    Fixed detailed_mp3_page

--- lib/HomeAndLanding/HomePage.dart
+++ lib/HomeAndLanding/HomePage.dart
@@ -665,7 +665,9 @@ class _Detailvol2State extends State<Detailvol2> {
   }
 
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
+    return SafeArea(
+      top: true,
+      minimum: EdgeInsets.only(top: 10),
       child: Container(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
@@ -680,44 +682,33 @@ class _Detailvol2State extends State<Detailvol2> {
           child: Container(
             color: Colors.black.withOpacity(0.1),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                SizedBox(
-                  height: MediaQuery.of(context).size.height / 25.06,
-                ),
                 FadeAnimation(
                   0.5,
-                  Align(
-                    alignment: Alignment(0, 0),
-                    child: new Image.asset('assets/1x/logoblanc.png',
-                        height: MediaQuery.of(context).size.height / 19.39),
-                  ),
+                  new Image.asset('assets/1x/logoblanc.png',
+                      height: MediaQuery.of(context).size.height / 19.39),
                 ),
-                SizedBox(height: MediaQuery.of(context).size.height / 30),
-                Padding(
-                  padding: EdgeInsets.only(
-                      left: MediaQuery.of(context).size.width / 6),
-                  child: Row(
-                    children: [
-                      FadeAnimation(
-                        0.5,
-                        Icon(Icons.headset,
-                            color: Colors.white,
-                            size: MediaQuery.of(context).size.height / 10.8),
-                      ),
-                      FadeAnimation(
-                        0.5,
-                        Text(widget.populars[widget.index],
-                            style: TextStyle(
-                                fontFamily: 'Raleway',
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: MediaQuery.of(context).size.height /
-                                    26.66)),
-                      ),
-                    ],
-                  ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    FadeAnimation(
+                      0.5,
+                      Icon(Icons.headset,
+                          color: Colors.white,
+                          size: MediaQuery.of(context).size.height / 10.8),
+                    ),
+                    FadeAnimation(
+                      0.5,
+                      Text(widget.populars[widget.index],
+                          style: TextStyle(
+                              fontFamily: 'Raleway',
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 23)),
+                    ),
+                  ],
                 ),
-                SizedBox(height: MediaQuery.of(context).size.height / 25.33),
                 FadeAnimation(
                   0.5,
                   Container(
@@ -737,99 +728,102 @@ class _Detailvol2State extends State<Detailvol2> {
                           ],
                           borderRadius: BorderRadius.circular(24))),
                 ),
-                SizedBox(height: MediaQuery.of(context).size.height / 21.33),
-                FadeAnimation(
-                  0.5,
-                  SliderTheme(
-                    data: SliderTheme.of(context).copyWith(
-                      trackHeight: 15,
-                      overlayColor: Colors.transparent,
-                      minThumbSeparation: 100,
-                      rangeThumbShape: RoundRangeSliderThumbShape(
-                        enabledThumbRadius: 10,
-                        disabledThumbRadius: 10,
-                      ),
-                    ),
-                    child: Slider.adaptive(
-                      value: _position.inSeconds.toDouble(),
-                      activeColor: Color(0xFF5F808F),
-                      inactiveColor: Colors.white,
-                      min: 0,
-                      max: _duration.inSeconds.toDouble(),
-                      onChanged: (double value) {
-                        setState(() {
-                          seekToSecond(value.toInt());
-                          value = value;
-                        });
-                      },
-                    ),
-                  ),
-                ),
-                FadeAnimation(
-                  0.5,
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                Padding(
+                  padding: EdgeInsets.only(left: 20),
+                  child: Wrap(
+                    runSpacing: 0,
+                    runAlignment: WrapAlignment.start,
                     children: [
-                      Padding(
-                        padding: EdgeInsets.only(left: 20),
-                        child: RichText(
-                          text: TextSpan(
-                            style: DefaultTextStyle.of(context).style,
-                            children: <TextSpan>[
-                              TextSpan(
-                                  text: _position.inMinutes
-                                          .remainder(60)
-                                          .toString()
-                                          .padLeft(2, "0") +
-                                      ':' +
-                                      _position.inSeconds
-                                          .remainder(60)
-                                          .toString()
-                                          .padLeft(2, "0"),
-                                  style: TextStyle(
-                                      fontFamily: 'nexa',
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize:
-                                          MediaQuery.of(context).size.height /
-                                              32)),
-                              TextSpan(
-                                  text: ' / ' + time[widget.index],
-                                  style: TextStyle(
-                                      fontFamily: 'nexa',
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.normal,
-                                      fontSize:
-                                          MediaQuery.of(context).size.height /
-                                              37.64)),
+                      FadeAnimation(
+                        0.5,
+                        SliderTheme(
+                          data: SliderTheme.of(context).copyWith(
+                            trackHeight: 15,
+                            overlayColor: Colors.transparent,
+                            minThumbSeparation: 100,
+                            rangeThumbShape: RoundRangeSliderThumbShape(
+                              enabledThumbRadius: 10,
+                              disabledThumbRadius: 10,
+                            ),
+                          ),
+                          child: Slider.adaptive(
+                            value: _position.inSeconds.toDouble(),
+                            activeColor: Color(0xFF5F808F),
+                            inactiveColor: Colors.white,
+                            min: 0,
+                            max: _duration.inSeconds.toDouble(),
+                            onChanged: (double value) {
+                              setState(() {
+                                seekToSecond(value.toInt());
+                                value = value;
+                              });
+                            },
+                          ),
+                        ),
+                      ),
+                      FadeAnimation(
+                        0.5,
+                        Padding(
+                          padding: EdgeInsets.only(left: 20),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              RichText(
+                                text: TextSpan(
+                                  style: DefaultTextStyle.of(context).style,
+                                  children: <TextSpan>[
+                                    TextSpan(
+                                        text: _position.inMinutes
+                                                .remainder(60)
+                                                .toString()
+                                                .padLeft(2, "0") +
+                                            ':' +
+                                            _position.inSeconds
+                                                .remainder(60)
+                                                .toString()
+                                                .padLeft(2, "0"),
+                                        style: TextStyle(
+                                            fontFamily: 'nexa',
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 17)),
+                                    TextSpan(
+                                        text: ' / ' + time[widget.index],
+                                        style: TextStyle(
+                                            fontFamily: 'nexa',
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.normal,
+                                            fontSize: 17)),
+                                  ],
+                                ),
+                              ),
+                              IconButton(
+                                  icon: Icon(iicon),
+                                  iconSize: 40,
+                                  color: Colors.white,
+                                  autofocus: true,
+                                  onPressed: () {
+                                    if (isplaying) {
+                                      _audio.pause();
+                                      setState(() {
+                                        isplaying = false;
+                                        iicon = MyFlutterApp.play_circle;
+                                      });
+                                    } else {
+                                      _au.play(audios[widget.index]);
+                                      setState(() {
+                                        isplaying = true;
+                                        iicon = MyFlutterApp.pause_circle;
+                                      });
+                                    }
+                                  }),
                             ],
                           ),
                         ),
                       ),
-                      IconButton(
-                          icon: Icon(iicon),
-                          iconSize: MediaQuery.of(context).size.height / 13,
-                          color: Colors.white,
-                          autofocus: true,
-                          onPressed: () {
-                            if (isplaying) {
-                              _audio.pause();
-                              setState(() {
-                                isplaying = false;
-                                iicon = MyFlutterApp.play_circle;
-                              });
-                            } else {
-                              _au.play(audios[widget.index]);
-                              setState(() {
-                                isplaying = true;
-                                iicon = MyFlutterApp.pause_circle;
-                              });
-                            }
-                          }),
                     ],
                   ),
                 ),
-                /* SizedBox(height: MediaQuery.of(context).size.height / 21.33),*/
               ],
             ),
           ),
